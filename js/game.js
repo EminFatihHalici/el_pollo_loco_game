c75c9a0cb067fa89de593ac94a0204e6f3b3d8f7
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-12-16 13:27:23 +0100

    feat: adding img to character and choicken in classes

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    console.log('My character is', character);
+    console.log('My character is', world.character);
     
     
 }
