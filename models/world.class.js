800bdfe1baa8989c0ffe9c152f603f00cd821256
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-12-16 14:06:37 +0100

    feat: drawing the chicken and character on canvas and also added clearRect and requestAnimationFrame

--- models/world.class.js
+++ models/world.class.js
@@ -1,8 +1,28 @@
 class World {
     character = new Character();
     enemies = [new Chicken(), new Chicken(), new Chicken()];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
 
     draw() {
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height); //Löscht die vorherigen Bilder im Canvas
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
+        //für das Zeichnen der Hühner als Array
+        this.enemies.forEach((enemy) => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+        //draw() wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
     }
 }
