ee114d4a6ea4a040c09a47119b1a88725cdb12c6
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-12-17 06:16:54 +0100

    style: scaled the charackter and chicken

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,10 @@ class Character extends MovableObject {
 
     constructor() {
        super().loadImage('img/2_character_pepe/2_walk/W-21.png')
+       this.width = 150;
+       this.height = 300;
+       this.x = 100;
+       this.y = 170;
     }
 
     jump() {
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,11 +1,12 @@
 class Chicken extends MovableObject {
 
-    y= 290;
-
       constructor() {
        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
 
        this.x = 250 +Math.random() * 500; //Zufällige Startposition der Hühner
+       this.width = 80;
+       this.height = 80;
+       this.y = 380;
     }
 
 }
