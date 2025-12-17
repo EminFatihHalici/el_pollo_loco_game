5058ef001f03ad46f22af36b512145912f2ed9c0
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-12-17 07:01:14 +0100

    feat: loading all walkin versions of character into json array

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,14 @@ class Character extends MovableObject {
 
     constructor() {
        super().loadImage('img/2_character_pepe/2_walk/W-21.png')
+       this.loadImages([
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+       ])
        this.width = 150;
        this.height = 300;
        this.x = 100;
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,12 +4,24 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = [];
 
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
+    //loading all img from character to json array
+    loadImages(arr) {
+        arr.forEach(path => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = path;
+       }); 
+
+    }
+
 
     moveRight() {
         console.log('Moving right');
