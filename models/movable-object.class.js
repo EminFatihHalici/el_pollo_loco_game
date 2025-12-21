9009062fadf50905151227cbbb4775d7b7fd46a7
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-12-21 06:42:56 +0100

    feat: calculating the time bof last hitting

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -11,6 +11,7 @@ class MovableObject {
     speedY = 0;
     accelaration = 3;
     energy = 100; 
+    lastHit = 0;
     offset = {
         top: 0,
         bottom: 0,
@@ -34,11 +35,19 @@ class MovableObject {
         this.energy -= 5;
         if (this.energy < 0) {
             this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime(); // letzter Zeitpunkt der Verletzung
         }
     }
 
     isDead() {
         return this.energy == 0;
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit; //difference in ms
+        timepassed = timepassed / 1000; // difference in sec
+        return timepassed < 1;
     }
 
     drawFrame(ctx) {
@@ -87,7 +96,7 @@ class MovableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentIMage % this.IMAGES_WALKING.length; // let i = 0 % 6; => Rest 0
+        let i = this.currentIMage % images.length; // let i = 0 % 6; => Rest 0
         // i = 0, 1, 2, 3, 4, 5, 0, 1, ...
         let path = images[i];
         this.img = this.imageCache[path];
