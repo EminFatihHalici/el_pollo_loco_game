374cf7112cafabe58e329c034c5793446a6e2fb8
Emin Fatih Halici <halicieminfatih@gmail.com>
2025-12-30 06:29:15 +0100

    feat: added endboss statuisbar class and images

---
+++ models/status-bar-endboss.class.js
@@ -0,0 +1,66 @@
+class StatusBarEndboss extends DrawableObject {
+
+    percentage = 100;
+
+
+    IMAGES = [
+        'img/7_statusbars/2_statusbar_endboss/blue/blue100.png',
+        'img/7_statusbars/2_statusbar_endboss/blue/blue80.png',
+        'img/7_statusbars/2_statusbar_endboss/blue/blue60.png',
+        'img/7_statusbars/2_statusbar_endboss/blue/blue40.png',
+        'img/7_statusbars/2_statusbar_endboss/blue/blue20.png',
+        'img/7_statusbars/2_statusbar_endboss/blue/blue0.png',
+    ]
+
+
+
+    constructor() {
+        super();
+        this.loadImages(this.IMAGES);
+        this.animate();
+        this.setPercantage(100);
+        this.y = 45;
+        this.x = 20;
+        this.width = 200;
+        this.height = 60;
+    }
+
+    animate() {
+        setInterval(() => {
+            // this.playAnimation(this.IMAGES);
+        }, 200);
+    }
+
+
+
+    setPercantage(percentage) {
+        this.percentage = percentage;
+        let imagePath = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[imagePath];
+
+    }
+
+
+    resolveImageIndex() {
+        if (this.percentage == 100) {
+            return 5;
+        } else if (this.percentage >= 80) {
+            return 4;
+        } else if (this.percentage >= 60) {
+            return 3;
+        } else if (this.percentage >= 40) {
+            return 2;
+        } else if (this.percentage >= 20) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+
+
+
+
+
+
+}
