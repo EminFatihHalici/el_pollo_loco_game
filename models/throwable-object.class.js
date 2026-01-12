b94d7dd2bda8fd15a506f387446e9f2ac1585f61
Emin Fatih Halici <halicieminfatih@gmail.com>
2026-01-12 14:22:30 +0100

    feat: added spinning bottle images and also implementing them, and changing the throw speed

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,23 +1,31 @@
 class ThrowableObject extends MovableObject {
 
+    IMAGES_ROTATE = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ]
+
     constructor(x, y) {
-        super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
+        this.loadImages(this.IMAGES_ROTATE);
         this.x = x;
         this.y = y;
         this.height = 60;
         this.width = 50;
-        this.throw();   
+        this.throw();
     }
 
     throw() {
         this.speedY = 15;
         this.applyGravity();
         setInterval(() => {
-            this.x += 10;
+            this.x += 15;
         }, 25);
-
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_ROTATE);
+        }, 100);
     }
-    
-
 
 }
