a476b59ab37dd2b93068d58204b17251f42cdab3
Emin Fatih Halici <halicieminfatih@gmail.com>
2026-01-14 05:36:28 +0100

    add: added diffretn situation picture (dead, hurt, walk)

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -8,6 +8,10 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ] 
+
+    IMAGES_DEAD = [
+      'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
     ]
 
 
@@ -15,6 +19,7 @@ class Chicken extends MovableObject {
        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
        this.x = x || 200 + Math.random() * 500; //Zufällige Startposition der Hühner
        this.loadImages(this.IMAGES_WALKING);
+       this.loadImages(this.IMAGES_DEAD);
        this.animate();
        this.speed = 0.15 + Math.random() * 0.25; //Zufällige Geschwindigkeit der Hühner
     }
--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -10,11 +10,16 @@ class SmallChicken extends MovableObject {
         'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ]
 
+    IMAGES_DEAD = [
+      'img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ]
+
 
       constructor(x) {
        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png')
        this.x = x + Math.random() * 500; //Zufällige Startposition der Hühner
        this.loadImages(this.IMAGES_WALKING);
+       this.loadImages(this.IMAGES_DEAD);
        this.animate();
        this.speed = 0.15 + Math.random() * 0.25; //Zufällige Geschwindigkeit der Hühner
        this.moveLeft();
