75adbc33ec270f66be4d3508875e3090ecfe8aa5
Emin Fatih Halici <halicieminfatih@gmail.com>
2026-02-11 06:37:08 +0100

    feat: add loading overlay with spinner and preload assets for smoother game start

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,58 +1,62 @@
 class DrawableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = [];
-    currentIMage = 0;
+  x = 120;
+  y = 280;
+  img;
+  height = 150;
+  width = 100;
+  imageCache = [];
+  currentIMage = 0;
 
+  loadImage(path) {
+    totalImages++;
+    this.img = new Image();
+    this.img.onload = () => {
+      imageLoaded();
+    };
+    this.img.src = path;
+  }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
+  draw(ctx) {
+    try {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height); // added try catch to handle image loading errors
+    } catch (e) {
+      console.warn("Error while drawing image: " + e);
+      console.log("Could nout load image", this.img.src);
     }
+  }
 
-    draw(ctx) {
-        try {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height); // added try catch to handle image loading errors
-        } catch (e) {
-        console.warn('Error while drawing image: ' + e);
-        console.log('Could nout load image', this.img.src);
-        }
-    }
+  //     drawFrame(ctx) {
+  //     // frame only for specific classes (instance of)
+  //     if (this instanceof Character || this instanceof Chicken || this instanceof Endboss || this instanceof SmallChicken) {
+  //         ctx.beginPath();
+  //         ctx.lineWidth = '2';
+  //         ctx.strokeStyle = 'blue';
+  //         ctx.rect(this.x, this.y, this.width, this.height);
+  //         ctx.stroke();
 
-    //     drawFrame(ctx) {
-    //     // frame only for specific classes (instance of)
-    //     if (this instanceof Character || this instanceof Chicken || this instanceof Endboss || this instanceof SmallChicken) {
-    //         ctx.beginPath();
-    //         ctx.lineWidth = '2';
-    //         ctx.strokeStyle = 'blue';
-    //         ctx.rect(this.x, this.y, this.width, this.height);
-    //         ctx.stroke();
+  //         ctx.beginPath();
+  //     ctx.lineWidth = '2';
+  //     ctx.strokeStyle = 'red';
+  //     ctx.rect(
+  //         this.x + this.offset.left,
+  //         this.y + this.offset.top,
+  //         this.width - this.offset.left - this.offset.right,
+  //         this.height - this.offset.top - this.offset.bottom
+  //         );
+  //     ctx.stroke();
+  //     }
+  // }
 
-    //         ctx.beginPath();
-    //     ctx.lineWidth = '2';
-    //     ctx.strokeStyle = 'red';
-    //     ctx.rect(
-    //         this.x + this.offset.left, 
-    //         this.y + this.offset.top, 
-    //         this.width - this.offset.left - this.offset.right, 
-    //         this.height - this.offset.top - this.offset.bottom
-    //         );
-    //     ctx.stroke();
-    //     }
-    // }
-
-    //loading all img from character to json array
-    loadImages(arr) {
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-
-    }
-
-
+  //loading all img from character to json array
+  loadImages(arr) {
+    totalImages += arr.length;
+    arr.forEach((path) => {
+      let img = new Image();
+      img.onload = () => {
+        imageLoaded();
+      };
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 }
